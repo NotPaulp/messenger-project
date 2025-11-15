b7b9e39e7484b9fb7500c13e0f2003c4bc015bac
Pavel <pavel@DESKTOP-GKHF695.localdomain>
2025-11-15 15:42:08 +0300

    RM unreleased stub for websocket

--- internal/handlers/messages/websocket.go
+++
@@ -1,9 +0,0 @@
-package messages
-
-import "net/http"
-
-// WebsocketHandler is a stub for websocket messages endpoint.
-func WebsocketHandler(w http.ResponseWriter, r *http.Request) {
-	// ...implementation to be added...
-	w.WriteHeader(http.StatusNotImplemented)
-}
